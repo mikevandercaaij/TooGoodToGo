1b699c198c4102530b5e405f6f648bdc2e94b0af
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-09-30 16:03:50 +0200

    Add onion structure

---
+++ Core.DomainServices.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Core.DomainServices.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Core.DomainServices/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.DomainServices
+{
+    public class Class1
+    {
+
+    }
+}
