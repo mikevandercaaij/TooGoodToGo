a299bb39cd94ef656e7e845f5b551c37f0c9027f
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-10-23 00:34:36 +0200

    finish styling for home page (logged in)

---
+++ Portal/ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+
+namespace Portal.ExtensionMethods
+{
+    public static class DateTimeExtensions
+    {
+        public static string GetGreetingMessage(this DateTime dateTime)
+        {
+            return dateTime.Hour switch
+            {
+                var hour when hour >= 0 && hour < 6 => "Goedenacht",
+                var hour when hour >= 6 && hour < 12 => "Goedemorgen",
+                var hour when hour >= 12 && hour < 18 => "Goedemiddag",
+                var hour when hour >= 18 && hour < 24 => "Goedenavond",
+                _ => "Hallo",
+            };
+        }
+    }
+}
+    
